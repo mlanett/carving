e1c4274615ecc0026fb0a83f0d592b26bda7ef33
Mark Lanett <mark.lanett@gmail.com>
2014-08-10 13:46:29 -0700

    WIP

--- spec/lib/carving_spec.rb
+++ spec/lib/carving_spec.rb
@@ -1,5 +1,25 @@
 require "spec_helper"
+require "stringio"
+
+class CarvingTest
+  include Carving
+  tag_logger :alpha
+end
 
 describe Carving do
-  it "has tests"
+  let(:buffer) { StringIO.new("") }
+  let(:logger) { ::Logger.new(buffer).tap { |my| my.formatter = ->(_,_,_,message) { [message, "\n"].join } } }
+
+  it "can tag a log with standalone tags" do
+    CarvingTest.logger :alpha
+    CarvingTest.new.logger info "Hello, world"
+    expect(buffer.string).to eq "Hello, world #alpha\n"
+  end
+
+  it "can tag a log with named tags and procs" do
+    CarvingTest.logger :alpha, beta: "123", gamma: ->() { 456 }
+    CarvingTest.new.logger info "Hello, world"
+    expect(buffer.string).to eq "Hello, world #alpha #beta:123 #gamma:456\n"
+  end
+
 end
